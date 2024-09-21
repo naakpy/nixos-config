2cfc2b1f66426f491ada52a8d3777fd7e45da6c9
Kaan Doyurur <kaan.doyurur@proton.me>
2024-09-21 16:02:23 +0200

    init: commit

---
+++ modules/packages.nix
@@ -0,0 +1,56 @@
+{ pkgs, ... }:
+
+{
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    (callPackage ./custom/cursor.nix {})
+    exegol
+    dbeaver-bin
+    easyeffects
+    burpsuite
+    obsidian
+    nano
+    vim
+    wget
+    curl
+    openssl
+    git
+    sysstat
+    lm_sensors
+    neofetch
+    xfce.thunar
+    ranger
+    pavucontrol
+    librewolf
+    btop
+    iotop
+    iftop
+    kitty
+    rofi-wayland
+    freetube
+    steam
+    cmake
+    gnumake
+    libgcc
+    gcc
+    xorg.xrandr
+    nodejs
+    yarn
+    zsh
+    unzip
+    python3
+    appimage-run
+    criterion
+    swww
+    networkmanagerapplet
+    mako
+    jq
+    brightnessctl
+    hyprshot
+    nomacs
+    firefox
+    rpi-imager
+    webcord
+  ];
+}
---
+++ modules/system.nix
@@ -0,0 +1,56 @@
+{ pkgs, lib, ... }:
+{
+  imports = [
+    ./packages.nix
+    ./pipewire.nix
+    ./hyprland.nix
+    ./fonts.nix
+    ./steam.nix
+    ./waybar.nix
+  ];
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.kaan = {
+    isNormalUser = true;
+    description = "kaan";
+    extraGroups = ["networkmanager" "wheel" "vboxusers" "docker"];
+    shell = pkgs.zsh;
+    ignoreShellProgramCheck = true;
+  };
+
+  # customise /etc/nix/nix.conf declaratively via `nix.settings`
+  nix.settings = {
+    # enable flakes globally
+    experimental-features = ["nix-command" "flakes"];
+  };
+
+  # do garbage collection weekly to keep disk usage low
+  nix.gc = {
+    automatic = lib.mkDefault true;
+    dates = lib.mkDefault "weekly";
+    options = lib.mkDefault "--delete-older-than 7d";
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable Network manager
+  networking.networkmanager.enable = true;
+
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.docker.rootless = {
+  enable = true;
+  setSocketVariable = true;
+  
+  };
+  programs.hyprland.xwayland.enable = true;
+}
+
+

84d04df1a8106f639d75df1cfbead43b7d99a867
Kaan Doyurur <kaan.doyurur@proton.me>
2024-12-10 02:40:21 +0100

    --wip-- [skip ci]

--- flake.nix
+++ flake.nix
@@ -2,32 +2,34 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";  # For Hyprland and other packages
+    home-manager.url = "github:nix-community/home-manager";  # Home Manager
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    hyprland.url = "github:hyprwm/Hyprland";  # Add Hyprland flake
+    hyprland.url = "github:hyprwm/Hyprland";  # Hyprland flake
   };
 
   outputs = inputs @ { self, nixpkgs, home-manager, hyprland, ... }: {
     nixosConfigurations = let
       makeHost = hostName: {
         system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
         modules = [
           ./hosts/${hostName}/hardware-configuration.nix
           ./modules/system.nix
+	  ./configuration.nix
 
           {
             wayland.windowManager.hyprland = {
               enable = true;
-              package = inputs.hyprland.packages.${nixpkgs.stdenv.hostPlatform.system}.hyprland;
+              package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
             };
           }
 
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = inputs;
-            home-manager.users.kaan = import ./home;
+            home-manager.extraSpecialArgs = inputs;  # Provide inputs to Home Manager
+            home-manager.users.kaan = import ./home;  # User-specific Home Manager config
           }
         ];
       };

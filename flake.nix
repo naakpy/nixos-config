c9bdb41f3f05220ff81404d9cdbfdf2478df3858
Kaan Doyurur <kaan.doyurur@proton.me>
2025-01-02 14:25:14 +0100

    --wip-- [skip ci]

--- flake.nix
+++ flake.nix
@@ -7,10 +7,6 @@
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = { self, nixpkgs, home-manager, ... } @ inputs: let

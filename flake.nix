507f02032f6404ff6509da876b1376e1e6de30b0
Kaan Doyurur <kaan.doyurur@proton.me>
2025-03-02 17:49:04 +0100

    --wip-- [skip ci]

--- flake.nix
+++ flake.nix
@@ -3,13 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    # Additional inputs
+    cursor.url = "github:omarcresp/cursor-flake/main";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... } @ inputs: let
+  outputs = { self, nixpkgs, home-manager, cursor, ... } @ inputs: let
     inherit (self) outputs;
   in {
     nixosConfigurations = {
@@ -19,6 +20,9 @@
           ./nixos/configuration.nix
           ./hosts/nixos-pc/host.nix
           ./hosts/nixos-pc/hardware-configuration.nix
+          ({ pkgs, ... }: {
+            environment.systemPackages = [ cursor.packages.${pkgs.system}.default ];
+          })
         ];
       };
 
@@ -28,6 +32,9 @@
           ./nixos/configuration.nix
           ./hosts/nixos-laptop/host.nix
           ./hosts/nixos-laptop/hardware-configuration.nix
+          ({ pkgs, ... }: {
+            environment.systemPackages = [ cursor.packages.${pkgs.system}.default ];
+          })
         ];
       };
     };

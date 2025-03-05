5fee8ce381edf7c23dda593f8b32f0826447a3c5
Kaan Doyurur <kaan.doyurur@proton.me>
2025-03-05 18:43:55 +0100

    --wip-- [skip ci]

--- home/programs/librewolf/default.nix
+++ home/programs/librewolf/default.nix
@@ -2,10 +2,13 @@
   programs.librewolf = {
     enable = true;
     settings = {
+      # Don't forget Canva Blocker addon
       "webgl.disabled" = false;
-      "privacy.resistFingerprinting" = false;
+
       "privacy.clearOnShutdown.history" = false;
       "privacy.clearOnShutdown.cookies" = false;
+      "privacy.clearOnShutdown.downloads" = false;
+
       "network.cookie.lifetimePolicy" = 0;
     };
   };

1139f8992edd8330b6b21680069bfb002e55b153
Kaan Doyurur <kaan.doyurur@proton.me>
2025-04-21 23:30:26 +0200

    --wip-- [skip ci]

--- hosts/nixos-pc/host.nix
+++ hosts/nixos-pc/host.nix
@@ -1,11 +1,27 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }: 
 
 {
   imports = [
     ./hardware-configuration.nix
     ./networking.nix
+    ../../nixos/greetd.nix
   ];
 
   networking.hostName = "nixos-pc";
-}
 
+  boot.initrd.systemd.enable = true;
+  boot.initrd.kernelModules = [
+    "tpm" "tpm_tis_core" "tpm_tis" "tpm_crb"
+  ];
+  boot.initrd.luks.devices.root = {
+    device = "/dev/nvme1n1p2";
+    crypttabExtraOpts = [ "tpm2-device=auto" "tpm2-pcrs=0" ];
+  };
+
+  services.greetd.settings.initial_session = {
+    user    = "kaan";
+    command = "${pkgs.hyprland}/bin/Hyprland";
+  };
+
+  services.greetd.restart = false;
+}
--- nixos/greetd.nix
+++ nixos/greetd.nix
@@ -1,33 +1,32 @@
-{
-  pkgs,
-  inputs,
-  ...
-}: let
-  tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
-  hyprland-session = "${pkgs.hyprland}/share/wayland-sessions";
+{ config, pkgs, ... }:
+
+let
+  tuigreet      = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+  hyprland_sess = "${pkgs.hyprland}/share/wayland-sessions";
 in {
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${tuigreet} --time --remember --remember-session --sessions ${hyprland-session}";
-        user = "greeter";
+        user    = "greeter";
+        command = ''
+          ${tuigreet}
+          --time
+          --remember
+          --remember-session
+          --sessions ${hyprland_sess}
+        '';
       };
     };
   };
 
-  # this is a life saver.
-  # literally no documentation about this anywhere.
-  # might be good to write about this...
-  # https://www.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
   systemd.services.greetd.serviceConfig = {
-    Type = "idle";
-    StandardInput = "tty";
-    StandardOutput = "tty";
-    StandardError = "journal"; # Without this errors will spam on screen
-    # Without these bootlogs will spam on screen
-    TTYReset = true;
-    TTYVHangup = true;
+    Type             = "idle";
+    StandardInput    = "tty";
+    StandardOutput   = "tty";
+    StandardError    = "journal";
+    TTYReset         = true;
+    TTYVHangup       = true;
     TTYVTDisallocate = true;
   };
 }

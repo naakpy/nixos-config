6b4f2dfc3251432e9afa917d4d14aaf55b674643
Kaan Doyurur <kaan@doyurur.xyz>
2025-04-24 04:42:25 +0200

    librewolf extensions

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    firefox-addons = { url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
   outputs = { self, nixpkgs, home-manager, ... } @ inputs: let
--- home/programs/librewolf/default.nix
+++ home/programs/librewolf/default.nix
@@ -1,18 +1,28 @@
+{ inputs, pkgs, config, ... }:
+
 {
   programs.librewolf = {
     enable = true;
+
     settings = {
       "webgl.disabled" = false;
-
       "privacy.clearOnShutdown.history" = false;
       "privacy.clearOnShutdown.cookies" = false;
       "privacy.clearOnShutdown.downloads" = false;
-
       "network.cookie.lifetimePolicy" = 0;
-
       "privacy.resistFingerprinting" = false;
       "privacy.fingerprintingProtection" = true;
       "privacy.fingerprintingProtection.overrides" = "+AllTargets,-CSSPrefersColorScheme,-JSDateTimeUTC";
+      "layout.css.prefers-color-scheme.content-override" = 0;
+    };
+
+    profiles.default = {
+      extensions = {
+        packages = with inputs.firefox-addons.packages.${pkgs.system}; [
+          ublock-origin
+          bitwarden
+        ];
+      };
     };
   };
 }
